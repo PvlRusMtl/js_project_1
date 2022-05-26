14893084c81030c0dbe097d32ef5a5914349f933
Rusty <rustam.zholdubayev@gmail.com>
2022-05-26 13:16:25 -0400

    cycles exercices

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 "use strict";
 
-let num = 50;
-
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num <= 55);
-
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    
+for (let i = 0; i < 3; i++) {
     console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+        
+    }
+}
+
+let res = '';
+const length = 7;
+
+for(let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        res += '*';
+    }
+    res += '\n';
+}
+
+console.log(res);
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`); 
+        for (let k = 0; k < 3; k++) {
+            if (k == 2) continue first;
+            console.log(`Third level: ${k}`);       
+        }
+    }      
+}
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+    
+let i = 2;
+while (i < 16) {
+    if (i%2 !== 0) {
+        console.log(i);
+    }    
+    i++;
 }
