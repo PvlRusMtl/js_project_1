dfc50ed17f41c92818c9bf22878de7eba60bce82
Rusty <rustam.zholdubayev@gmail.com>
2022-05-29 18:20:20 -0400

    exercice3

--- lesson.js
+++ lesson.js
@@ -8,18 +8,6 @@ for (let i = 0; i < 3; i++) {
     }
 }
 
-let res = '';
-const length = 7;
-
-for(let i = 1; i < length; i++) {
-    for (let j = 0; j < i; j++) {
-        res += '*';
-    }
-    res += '\n';
-}
-
-console.log(res);
-
 first: for (let i = 0; i < 3; i++) {
     console.log(`First level: ${i}`);
     for (let j = 0; j < 3; j++) {
@@ -58,8 +46,30 @@ const data = [5, 10, 'Shopping', 20, 'Homework'];
 const result = [];
 
 for (let i = data.length - 1; i >= 0; i--) {
+    
     for (let j = 0; j <= i; j++) {
         result[j] = data[i];
-}
+    }
+    
 }
 console.log(result)
+
+//
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i];
+}
+console.log(result);
+//
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+for (let i = 0; i < data.length; i++) {
+    if (typeof(data[i]) === 'number') {
+        data[i] = data[i] * 2;
+    } else if (typeof(data[i] === 'string')) {
+        data[i] = `${data[i]} - done`;
+    console.log(data);
+    }
+}
+
+
--- script.js
+++ script.js
@@ -1,34 +1,107 @@
-"use strict";
+// 'use strict';
 
-const numOfFilms = +prompt('How many movies have you seen?');
+// const usdCurr = 28;
+// const discount = 0.9;
 
-const personalMovieDb = {
-    count: numOfFilms,
+// function convert(amount, curr){
+//     return curr * amount;
+    
+// }
+
+// function promotion(result) {
+//     console.log(result * discount);
+// }
+// promotion(convert(500, usdCurr));
+
+// function test() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) return
+//     }
+//     console.log('done');
+// }
+// test();
+
+// function doNothing() {};
+// console.log(doNothing() === undefined);
+
+
+// function getMathResult(base, repeat) {
+//     if (typeof(repeat) != 'number' || repeat <= 0) {
+//         return base;
+//     } else {
+//         let str = '';
+//         for (let i = 1; i <= repeat; i++){
+//             if (i === repeat) {
+//                 str += `${base * i}`;                
+//             } else {
+//                 str += `${base * i}---`;                
+//             }        
+//         }
+//         return str;
+//     }
+// }
+// getMathResult(3, -3);
+// //
+let numberOfFilms;
+function start() {
+    numberOfFilms = prompt('How many movies have you seen?');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('How many movies have you seen?');
+    }
+}
+start();
+const personalMovieDB = {
+    count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: {},
+    genres: [],
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Last of watched movies?", ''),
-          b = prompt('How could you evaluate it?', '');
-    
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDb.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Last movie?', ''),
+              b = prompt('How could you evaluate it?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
-if (personalMovieDb.count < 10) {
-    console.log("Too little movies");
-} else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
-    console.log("Classic");
-} else if (personalMovieDb.count >= 30) {
-    console.log("Kinoman");
-} else {
-    console.log("Error");
+
+// rememberMyFilms();
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10) {
+        console.log('too little');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('classic');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('kinoman');
+    } else {
+        console.log('error');
+    }
 }
-console.log(personalMovieDb);
+
+// detectPersonalLevel();
+
+function showMyDB(hidden){
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i-1] = prompt(`Your favorite genre number ${i}?`, '');
+        } 
+}
+
+writeYourGenres();
