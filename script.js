1324eea69bc57325bb0e1bc10fdd38435e01a155
Rusty <rustam.zholdubayev@gmail.com>
2022-05-31 13:26:41 -0400

    35 transmit by value or link and spread operator lesson

--- script.js
+++ script.js
@@ -64,45 +64,78 @@
 
 // // writeYourGenres();
 
-const arr = [2, 13, 26, 8, 10];
-arr.sort(compareNum);
-console.log(arr);
-function compareNum(a, b){
-    return a-b;
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+// const copy = obj;
+// copy.a = 10;
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
 }
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
 
-// arr[99] = 0;
-// console.log(arr.length);
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} внутри массива ${arr}`);
-// });
+const newNumbers = copy(numbers);
+newNumbers.a  = 10;
+newNumbers.c.x = 10;
+// console.log(newNumbers);
+// console.log(numbers);
 
-// arr.pop();
-// arr.push(10);
+const add = {
+    d: 17,
+    e: 20
+};
+const clone = Object.assign({}, add);
+clone.d = 20;
+// console.log(add);
+// console.log(clone);
 
-// console.log(arr);
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+newArray[1] = 'skjdldsl';
+console.log(newArray);
+console.log(oldArray);
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
-// for (let value of arr) {
-//     console.log(value);
-// }
-// const str = prompt("", "");
-// const products = str.split(", ");
-// products.sort();
-// console.log(products.join('; '));
-// console.log(Object.keys(options).length);
-// let counter = 0;
-// for (let key in options ) {
-//     if (typeof(options[key]) === 'object') {
-//         for(let i in options[key]) {
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-            
-//         }
-//     } else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//         counter++;
-//     }
-// }
-// console.log(counter);
+const video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'blogger'],
+    internet = [...video, ...blogs, 'vk', 'fb'];
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+const num = [2, 5, 7];
+log(...num);
+
+const array = ['a', 'b'];
+const newAarray = [...array];
+const q = {
+    one: 1,
+    two: 2
+};
+const newObj = {...q};
