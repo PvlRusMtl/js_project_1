06b098464d6ed11304096dce154d624b5d9f4f20
Rusty <rustam.zholdubayev@gmail.com>
2022-06-02 12:10:30 -0400

    36 oop

--- script.js
+++ script.js
@@ -80,62 +80,29 @@
 // console.log(copy);
 // console.log(obj);
 
-function copy(mainObj) {
-    let objCopy = {};
+let str = 'some';
+let strObj = new String(str);
 
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    say: function() {
+        console.log('Hello');
     }
 };
 
-const newNumbers = copy(numbers);
-newNumbers.a  = 10;
-newNumbers.c.x = 10;
-// console.log(newNumbers);
-// console.log(numbers);
+const john = Object.create(soldier);
 
-const add = {
-    d: 17,
-    e: 20
-};
-const clone = Object.assign({}, add);
-clone.d = 20;
-// console.log(add);
-// console.log(clone);
+// const john = {
+//     health: 100
+// };
 
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-newArray[1] = 'skjdldsl';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejournal', 'blogger'],
-    internet = [...video, ...blogs, 'vk', 'fb'];
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-const num = [2, 5, 7];
-log(...num);
-
-const array = ['a', 'b'];
-const newAarray = [...array];
-const q = {
-    one: 1,
-    two: 2
-};
-const newObj = {...q};
+// // john.__proto__ = soldier;
+// Object.setPrototypeOf(john, soldier);
+// // console.log(john.armor);
+john.say();
