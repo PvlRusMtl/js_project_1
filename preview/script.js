c50320269fc31bd5b248e34b8ec25a1f36d0050b
Rusty <rustam.zholdubayev@gmail.com>
2022-06-14 12:42:01 -0400

    lesson 59 map

--- preview/script.js
+++ preview/script.js
@@ -9,54 +9,48 @@ const user = {
     }
 };
 
-// for(const key of user) {
-//     console.log(user[key]);
+const userMap = new Map(Object.entries(user));
+
+const newUserObj = Object.fromEntries(userMap);
+console.log(newUserObj);
+
+const shops = [
+    {rice: 500},
+    {oil: 200},
+    {bread: 50}
+];
+
+const budget = [5000, 15000, 25000];
+const map = new Map([
+    [{paper: 400}, 8000]
+]);
+
+shops.forEach((shop, i) => {
+    map.set(shop, budget[i]);
+});
+
+console.log(map);
+// console.log(map.get(shops[0]));
+// console.log(map.has(shops[0]));
+// map.delete(key);
+// map.clear();
+// map.size;
+// map.keys();
+
+// const goods = [];
+// for (let shop of map.keys()) {
+//     goods.push(Object.keys(shop)[0]);
+// }
+// console.log(goods);
+
+// for (let price of map.values()) {
+//     console.log(price);
 // }
 
-const arr = ['b', 'a', 'c'];
-Array.prototype.someMethod = function() {};
-
-console.dir(arr);
-
-for(const key of arr) {
-    console.log(key);
-}
-
-// const str = 'string';
-
-// for(const key in str) {
-//     console.log(str[key]);
+// for (let [shop, price] of map.entries()) {
+//     console.log(price, shop);
 // }
 
-const salaries = {
-    john: 500,
-    ivan: 1000,
-    ann: 5000,
-    sayHello: function() {
-        console.log('Hello');
-    }
-};
-
-salaries[Symbol.iterator] = function() {
-    return {
-        current: this.john,
-        last: this.ann,
-
-        next() {
-            if(this.current < this.last) {
-                this.current = this.current + 500;
-                return {done: false, value: this.current}
-            } else {
-                return {done: true}
-            }
-           
-        }
-    }
-}
-
-const iterator = salaries[Symbol.iterator]();
-console.log(iterator.next());
-
-// for (let res of salaries) {
-//     console.log(res);
-// }
+// map.forEach((value, key, map) => {
+//     console.log(key, value);
+// })
