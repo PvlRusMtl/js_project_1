7f8ce43cd5edf9664bf8f94ce2c347a775a8f24b
Rusty <rustam.zholdubayev@gmail.com>
2022-06-20 12:57:57 -0400

    lesson 70 practice passed date

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -43,12 +43,20 @@ window.addEventListener('DOMContentLoaded', () => {
   const deadline = '2022-06-22';
 
   function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
           days = Math.floor(t / (1000 * 60 * 60 * 24)),
           hours = Math.floor((t / 1000 * 60 * 60) % 24),
           minutes = Math.floor((t / 1000 / 60) % 60),
           seconds = Math.floor((t / 1000) % 60);
-    
+    }
 
     return {
       'total': t,
@@ -62,9 +70,7 @@ window.addEventListener('DOMContentLoaded', () => {
   function getZero(num) {
     if (num >= 0 && num < 10) {
       return `0${num}`;
-    } else if (num < 0) {
-      return "00";
-    } else {
+    }  else {
       return num;
     }
   }
