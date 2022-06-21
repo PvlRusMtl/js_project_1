78d56f271589d122d6167326a38d7d7f508140a1
Rusty <rustam.zholdubayev@gmail.com>
2022-06-21 12:59:09 -0400

    lesson 74 mutationobserver, resizeobserver, contenteditable

--- preview/script.js
+++ preview/script.js
@@ -1,26 +1,11 @@
 'use strict';
 
-const box = document.querySelector('.box'),
-      btn = document.querySelector('button');
+const box = document.querySelector('.box');
 
-// const width = box.clientWidth;
-// const height = box.clientHeight;
-// const width = box.offsetWidth;
-// const height = box.offsetHeight;
-const width = box.scrollWidth;
-const height = box.scrollHeight;
-
-console.log(width, height);
-
-btn.addEventListener('click', () => {
-    box.style.height = box.scrollHeight + 'px';
-    // console.log(box.scrollTop);
+let observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords);
 });
 
-console.log(box.getBoundingClientRect().top);
-
-const style = window.getComputedStyle(box);
-
-console.log(style.display);
-
-console.log(document.documentElement.scrollTop);
+observer.observe(box, {
+    childList: true
+})
