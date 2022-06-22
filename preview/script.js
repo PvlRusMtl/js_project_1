ce1f806e6a84410318482a4412cfefc1906722dd
Rusty <rustam.zholdubayev@gmail.com>
2022-06-22 11:23:47 -0400

    lesson 75 functions-constructors

--- preview/script.js
+++ preview/script.js
@@ -1,11 +1,25 @@
 'use strict';
 
-const box = document.querySelector('.box');
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
+}
 
-let observer = new MutationObserver(mutationRecords => {
-    console.log(mutationRecords);
-});
+User.prototype.exit = function(name) {
+    console.log(`User ${this.name} is logged out`);
+};
 
-observer.observe(box, {
-    childList: true
-})
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
