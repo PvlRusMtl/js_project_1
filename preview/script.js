8b42c5cf3ac74af7a1244d1c27312ec0ad85ce07
Rusty <rustam.zholdubayev@gmail.com>
2022-06-26 18:57:41 -0400

    lesson 82 json, deep copy

--- preview/script.js
+++ preview/script.js
@@ -1,35 +1,15 @@
 'use strict';
 
-class Rectangle {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
+const persone = {
+    name: 'Alex',
+    tel: '+74444444',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
     }
+};
 
-    calcArea() {
-        return this.height * this.width;
-    }
-}
-
-class ColoredRecyangleWithText extends Rectangle {
-    constructor(height, width, text, bgColor) {
-        super(height, width);
-        this.text = text;
-        this.bgColor = bgColor;
-    }
-
-    showMyProps() {
-        console.log(`Text: ${this.text}, color: ${this.bgColor}`);
-    }
-}
-
-const div = new ColoredRecyangleWithText(25, 10, 'Hello World', 'red');
-
-div.showMyProps();
-console.log(div.calcArea());
-
-// const square = new Rectangle(10, 10);
-// const long = new Rectangle(20, 100);
-
-// console.log(long.calcArea());
-// console.log(square.calcArea());
+const clone = JSON.parse(JSON.stringify(persone));
+clone.parents.mom = 'Ann';
+console.log(persone);
+console.log(clone);
