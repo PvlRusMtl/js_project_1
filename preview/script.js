ceaaf51f8cf410cc227f479188d949f356dc1d86
Rusty <rustam.zholdubayev@gmail.com>
2022-07-05 12:32:11 -0400

    99 getters and setters

--- preview/script.js
+++ preview/script.js
@@ -1,34 +1,17 @@
 'use strict';
 
-// new RegExp('pattern', 'flags');
-// /pattern/ f
+const persone = {
+    name: 'Alex',
+    age: 25,
 
-// const ans = prompt('Tape your number');
+    get userAge() {
+        return this.age;
+    },
 
-// const reg = /\d/g;
-// console.log(ans.match(reg));
+    set userAge(num) {
+        this.age = num;
+    }
+};
 
-const str = 'My name is R2D2';
-
-console.log(str.match(/\D/ig));
-
-//\D
-//\W
-
-// \d
-// \w
-// \s
-
-// i
-// g
-// m
-
-// console.log(ans.search(reg));
-// console.log(ans.match(reg));
-
-// const pass = prompt('Password');
-
-// console.log(pass.replace(/\./g, "*"));
-
-// console.log('12-34-56'.replace(/-/g, ':'));
-
+console.log(persone.userAge = 30);
+console.log(persone.userAge);
