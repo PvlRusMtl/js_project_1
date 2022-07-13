dc8512d7240045b6fb686f6541757ef9dea20db3
Rusty <rustam.zholdubayev@gmail.com>
2022-07-13 14:55:43 -0400

    106 try/catch

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 'use strict';
 
+try {
+    document.querySelector('.active').addEventListener('click', () => {
+        console.log('click');
+    });
+} catch(e) {}
+
+console.log('ok');
+
+// try {
+//     console.log('Hey');
+//     console.log(a);
+//     console.log('result');
+// } catch(error) {
+//     console.log(error.name);
+//     console.log(error.message);
+//     console.log(error.stack);
+// } finally {
+
+// }
+
+// console.log('still ok');
+
 // const personalMovieDB = {
 //     count: 0,
 //     movies: {},
@@ -76,59 +98,3 @@
     
 // };
 
-const restorantData = {
-    menu: [
-        {
-            name: 'Salad Caesar',
-            price: '14$'
-        },
-        {
-            name: 'Pizza Diavola',
-            price: '9$'
-        },
-        {
-            name: 'Beefsteak',
-            price: '17$'
-        },
-        {
-            name: 'Napoleon',
-            price: '7$'
-        }
-    ],
-    waitors: [
-        {name: 'Alice', age: 22}, {name: 'John', age: 24}
-    ],
-    averageLunchPrice: '20$',
-    openNow: true
-};
-
-function isOpen(prop) {
-    let answer = '';
-    prop ? answer = 'Открыто' : answer = 'Закрыто';
-
-    return answer;
-}
-
-console.log(isOpen(restorantData.openNow));
-
-function isAverageLunchPriceTrue(fDish, sDish, average) {
-    console.log(+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)), +average.slice(0, -1));
-    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < average.slice(0, -1)) {
-        return 'Цена ниже средней';
-    } else {
-        return 'Цена выше средней';
-    }
-}
-
-console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
-
-function transferWaitors(data) {
-    const copy = JSON.parse(JSON.stringify(data));
-
-    copy.waitors[0] = {name: 'Mike', age: 32};
-    console.log(copy.waitors);
-    return copy;
-}
-
-transferWaitors(restorantData);
-console.log(restorantData.waitors);
