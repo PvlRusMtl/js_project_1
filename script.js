848c0ab87badd12b61e7e80f9ab4b9b6c43eec25
Rusty <rustam.zholdubayev@gmail.com>
2022-07-14 14:32:19 -0400

    109 libraries and frameworks

--- script.js
+++ script.js
@@ -1,100 +1,21 @@
 'use strict';
 
-try {
-    document.querySelector('.active').addEventListener('click', () => {
-        console.log('click');
-    });
-} catch(e) {}
+// Angular:
+// -node.js
+// -TypeScript
+// -Webpack
+// -MVC pattern
+// -Angular
 
-console.log('ok');
+// React:
+// -JSX
+// -Babel
+// -Webpack
+// -React
+// -node.js
 
-// try {
-//     console.log('Hey');
-//     console.log(a);
-//     console.log('result');
-// } catch(error) {
-//     console.log(error.name);
-//     console.log(error.message);
-//     console.log(error.stack);
-// } finally {
-
-// }
-
-// console.log('still ok');
-
-// const personalMovieDB = {
-//     count: 0,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false,
-//     start: function() {
-//         personalMovieDB.count = prompt('How many movies have you seen?');
-    
-//         while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-//             personalMovieDB.count = prompt('How many movies have you seen?');
-//         }
-//     },
-//     rememberMyFilms: function(){
-//         for (let i = 0; i < 2; i++) {
-//             const a = prompt('Last movie?', '').trim(),
-//                   b = prompt('How could you evaluate it?', '');
-//             if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//                 personalMovieDB.movies[a] = b;
-//                 console.log('done');
-//             } else {
-//                 console.log('error');
-//                 i--;
-//             }
-//         }
-//     },
-//     detectPersonalLevel: function(){
-//         if (personalMovieDB.count < 10) {
-//             console.log('too little');
-//         } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//             console.log('classic');
-//         } else if (personalMovieDB.count >= 30) {
-//             console.log('kinoman');
-//         } else {
-//             console.log('error');
-//         }
-//     },
-//     showMyDB: function(hidden){
-//         if (!hidden) {
-//             console.log(personalMovieDB);
-//         }
-//     },
-//     toggleVisibleMyDB: function() {
-//         if (personalMovieDB.privat) {
-//             personalMovieDB.privat = false;
-//         } else {
-//             personalMovieDB.privat = true;
-//         }
-//     },
-//     writeYourGenres: function() {
-//         for (let i = 1; i < 2; i++) {
-//             // let genre = prompt(`Your favorite genre number ${i}`);
-//             // if (genre === "" || genre === null) {
-//             //     console.log('Not correct or data abcence');
-//             //     i--;
-//             // } else {
-//             //     personalMovieDB.genres[i-1] = genre;
-//             // }
-
-//             let genres = prompt(`Tape your favorites genres number with comma`).toLowerCase();
-//             if (genres === "" || genres === null) {
-//                 console.log('Not correct or data abcence');
-//                 i--;
-//             } else {
-//                 personalMovieDB.genres = genres.split(", ");
-//                 personalMovieDB.genres.sort();
-//             }
-            
-//         } 
-//         personalMovieDB.genres.forEach((item, i) => {
-//             console.log(`Любимый жанр ${i + 1} - это ${item}`);
-//         });
-//     }
-    
-// };
+// Vue.js:
+// -Webpack
+// -Vue
+// -node.js
 
